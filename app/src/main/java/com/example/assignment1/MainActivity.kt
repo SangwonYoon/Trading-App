92d8590181e1a11171ad9b8b0857c4db68a79043
SangwonYoon <yoonsw0532@naver.com>
2021-10-28 04:26:52 +0900

    로그인 기능, 회원 로그인 여부 기억
    회원으로 로그인 했는지 여부를 저장하는 isMember 변수 추가
    로그인 시 아이디, 비밀번호 대조 기능 추가
    비회원으로 상품 보기 버튼 클릭 시 isMember 변수 값 false로 바꿔주는 내용 추가

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -7,7 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
+internal var isMember = false
 class MainActivity : AppCompatActivity() {
     private var pref: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +24,33 @@ class MainActivity : AppCompatActivity() {
         Log.e("TAG", "쉐어드에 저장된 아이디 = " + pref?.getString("id", ""))
         Log.e("TAG", "쉐어드에 저장된 비밀번호 = " + pref?.getString("password", ""))
 
-        var signInButton : Button = findViewById(R.id.signIn)
+        val logInButton : Button = findViewById(R.id.logIn)
+        val idTextView : TextView = findViewById(R.id.memberID)
+        val pwTextView : TextView = findViewById(R.id.memberPassword)
+
+        logInButton.setOnClickListener{
+            val idInput = idTextView.text.toString()
+            val pwInput = pwTextView.text.toString()
+            if(idInput == id && pwInput == password){
+                isMember = true
+                val intent = Intent(this, ProductActivity::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(this,"아이디 또는 비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val signInButton : Button = findViewById(R.id.signIn)
 
         signInButton.setOnClickListener{
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
 
-        var noLogInButton: Button = findViewById(R.id.noLogIn)
+        val noLogInButton: Button = findViewById(R.id.noLogIn)
 
         noLogInButton.setOnClickListener {
+            isMember = false
             val intent = Intent(this, ProductActivity::class.java)
             startActivity(intent)
         }
